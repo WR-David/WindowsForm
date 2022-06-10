a83519cd1482533e98ac4a95d02896d8a00ecf8a
David <david.riveraf21@gmail.com>
2022-06-10 02:24:39 -0400

    Second commit

---
+++ WindowsFormsApp1/Model/Connection.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1.Model
+{
+    internal class Connection
+    {
+        public MySqlConnection con;
+        public MySqlConnection conectar()
+        {
+            string host = "localhost";
+            string db = "prograavanzada";
+            string user = "root";
+            string clave = "12345678";
+            string conecta = "server = " + host + "; user id = " + user + "; database = " + db
+           + "; password = " + clave;
+            con = new MySqlConnection(conecta);
+            return con;
+        }
+    }
+}
